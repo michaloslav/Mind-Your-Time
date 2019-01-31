a7bcc6c1797a55aa0b42288df852b0b39c0d7e66
michaloslav <31165844+michaloslav@users.noreply.github.com>
2019-01-31 19:59:53 +0100

    Updates

---
+++ precache-manifest.326d5dd81d3210bb9dd2423420c25fde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e707b13a0b7f77898260",
+    "url": "/static/js/main.e707b13a.chunk.js"
+  },
+  {
+    "revision": "424b3a5be432946e2bfb",
+    "url": "/static/js/1.424b3a5b.chunk.js"
+  },
+  {
+    "revision": "e707b13a0b7f77898260",
+    "url": "/static/css/main.c9597dc1.chunk.css"
+  },
+  {
+    "revision": "1302e047ab17e01f7c7c19ed8c3571f2",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Mind-Your-Time/precache-manifest.3ad4317dc0fdad9addb44610a3b5206d.js"
+  "/precache-manifest.326d5dd81d3210bb9dd2423420c25fde.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Mind-Your-Time/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
