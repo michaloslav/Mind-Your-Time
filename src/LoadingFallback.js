92496f8e81bbb20eccd5356e6d75a0cd9bea11a3
michaloslav <31165844+michaloslav@users.noreply.github.com>
2019-06-12 21:40:53 +0200

    Making LoadingFallback only show the spinner after a while

--- src/LoadingFallback.js
+++ src/LoadingFallback.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Spinner from 'react-md-spinner';
 
-const LoadingFallback = props => <Spinner singleColor="#039be5"/>
+export default class LoadingFallback extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      show: false,
+      showTimeout: setTimeout(() => this.setState({show: true}), 100)
+    }
+  }
 
-export default LoadingFallback
+  componentWillUnmount(){
+    clearTimeout(this.state.showTimeout)
+  }
+
+  render(){
+    if(this.state.show) return <Spinner singleColor="#039be5"/>
+    else return null
+  }
+}
